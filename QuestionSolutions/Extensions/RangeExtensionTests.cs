f6186b63d1b2befdaa87f39baab4eade18a14736
Kendrick Choate <kendrickchoate@gmail.com>
2023-02-08 21:40:58 -0600

    Add tests for Contains taking a range input.

--- QuestionSolutions/Extensions/RangeExtensionTests.cs
+++ QuestionSolutions/Extensions/RangeExtensionTests.cs
@@ -66,4 +66,102 @@ public class RangeExtensionTests
         // Assert
         result.Should().BeFalse();
     }
+
+    [Fact]
+    public void Contains_WhenInputRangeIsWithinRange_ReturnsTrue()
+    {
+        // Arrange
+        var input = 2..3;
+        var range = 1..4;
+
+        // Act
+        var result = range.Contains(input);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Contains_WhenInputRangeStartEqualsRangeStart_AndEndsBeforeRangeEnd_ReturnsTrue()
+    {
+        // Arrange
+        var input = 1..3;
+        var range = 1..4;
+
+        // Act
+        var result = range.Contains(input);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Contains_WhenInputRangeStartsAfterRangeStart_AndEndsOnRangeEnd_ReturnsTrue()
+    {
+        // Arrange
+        var input = 2..4;
+        var range = 1..4;
+
+        // Act
+        var result = range.Contains(input);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Contains_WhenInputRangeIsSameAsRange_ReturnsTrue()
+    {
+        // Arrange
+        var input = 1..4;
+        var range = 1..4;
+
+        // Act
+        var result = range.Contains(input);
+
+        // Assert
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Contains_WhenInputRangeStartsBeforeRange_ReturnsTrue()
+    {
+        // Arrange
+        var input = 0..3;
+        var range = 1..4;
+
+        // Act
+        var result = range.Contains(input);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Contains_WhenInputRangeEndsAfterRange_ReturnsTrue()
+    {
+        // Arrange
+        var input = 2..5;
+        var range = 1..4;
+
+        // Act
+        var result = range.Contains(input);
+
+        // Assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public void Contains_WhenInputRangeStartsBeforeRange_AndEndsAfterRange_ReturnsTrue()
+    {
+        // Arrange
+        var input = 0..5;
+        var range = 1..4;
+
+        // Act
+        var result = range.Contains(input);
+
+        // Assert
+        result.Should().BeFalse();
+    }
 }

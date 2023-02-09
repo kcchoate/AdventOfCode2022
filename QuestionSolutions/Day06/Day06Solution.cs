c8ebd95e0bf885022de0f178530a716f075bef8d
Kendrick Choate <kendrickchoate@gmail.com>
2023-02-08 21:58:58 -0600

    Implement basic solution for basic test cases.

--- QuestionSolutions/Day06/Day06Solution.cs
+++ QuestionSolutions/Day06/Day06Solution.cs
@@ -2,5 +2,36 @@ namespace QuestionSolutions.Day06;
 
 public class Day06Solution
 {
-    
+    public int FindStartOfPacketMarker(string input)
+    {
+        var first = input[0];
+        var second = input[1];
+        var third = input[2];
+        var fourth = input[3];
+        var result = 4;
+
+        while (IsCharacterShared(first, second, third, fourth))
+        {
+            result++;
+            first = input[result - 4];
+            second = input[result - 3];
+            third = input[result - 2];
+            fourth = input[result - 1];
+        }
+
+        return result;
+    }
+
+    private bool IsCharacterShared(char first, char second, char third, char fourth)
+    {
+        var set = new HashSet<char>
+        {
+            first,
+            second,
+            third,
+            fourth
+        };
+
+        return set.Count != 4;
+    }
 }
--- QuestionSolutions/Day06/Day06Tests.cs
+++ QuestionSolutions/Day06/Day06Tests.cs
@@ -2,5 +2,20 @@ namespace QuestionSolutions.Day06;
 
 public class Day06Tests
 {
-    
+    private readonly Day06Solution _sut = new();
+
+    [Theory]
+    [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+    [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+    [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+    public void FindStartOfPacketMarker_GivenBasicInput_ReturnsCorrectMarker(string input, int expected)
+    {
+        // Act
+        var result = _sut.FindStartOfPacketMarker(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
 }

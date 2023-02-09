20c78d791a9b5bf6661eb2e1b5578da217b0dd57
Kendrick Choate <kendrickchoate@gmail.com>
2023-02-08 22:08:45 -0600

    Refactor solution to use a shared method.

--- QuestionSolutions/Day06/Day06Solution.cs
+++ QuestionSolutions/Day06/Day06Solution.cs
@@ -2,30 +2,14 @@ namespace QuestionSolutions.Day06;
 
 public class Day06Solution
 {
-    public int FindStartOfPacketMarker(string input)
+    public int FindStartOfPacketMarker(string input) => FindStartOfDistinctChars(input, 4);
+
+    public int FindStartOfMessageMarker(string input) => FindStartOfDistinctChars(input, 14);
+
+    private int FindStartOfDistinctChars(string input, int requiredLength)
     {
-        var first = input[0];
-        var second = input[1];
-        var third = input[2];
-        var fourth = input[3];
-        var result = 4;
-
-        while (IsCharacterShared(first, second, third, fourth))
-        {
-            result++;
-            first = input[result - 4];
-            second = input[result - 3];
-            third = input[result - 2];
-            fourth = input[result - 1];
-        }
-
-        return result;
-    }
-
-    public int FindStartOfMessageMarker(string input)
-    {
-        var result = 14;
-        while (IsCharacterShared(input.Skip(result - 14).Take(14).ToArray() ))
+        var result = requiredLength;
+        while (IsCharacterShared(input.Skip(result - requiredLength).Take(requiredLength).ToArray() ))
         {
             result++;
         }
